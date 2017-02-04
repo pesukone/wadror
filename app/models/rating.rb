6b6bd9e8d15eb0e26a75d0addbf6fc9d5438a10a
Jussi Aalto <jussi.aalto@cs.helsinki.fi>
2017-02-04 16:59:05 +0200

    tehty kolmosviikkoa

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,7 +1,8 @@
 class Rating < ApplicationRecord
 	belongs_to :beer
+	belongs_to :user 	# rating kuuluu myös käyttäjään
 
 	def to_s
-		beer.name + " " + score.to_s
+		"#{beer.name} #{score}"
 	end
 end
